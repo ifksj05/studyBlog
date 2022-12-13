5480cc8f1c6eb612181363bd44ed2734fa78a2da
ksj <gksjksjksjks@gmail.com>
2022-12-13 20:02:05 +0900

    dayoff:221213

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,64 +1,30 @@
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
 
-	private static boolean hansooDiscriminant(int num) {
-		// TODO Auto-generated method stub
-		boolean returnThis = false;
-
-		String stnum = num + "";
-		String[] stnumArr = stnum.split("");
-		int[] intnumArr = new int[stnumArr.length];
-		for (int i = 0; i < intnumArr.length; i++) {
-//			intnumArr[i] = (int)stnumArr[i] ;
-			intnumArr[i] = Integer.parseInt(stnumArr[i]);
-		}
-
-		if (stnumArr.length == 1) { // 1자리인 경우 무조건 등차수열
-			returnThis = true;
-
-		} else { // 2자리 ~ n자리 숫자
-			if (intnumArr[1] - intnumArr[0] > 0) { // 증가중
-				for (int i = 0; i < stnumArr.length - 1; i++) {
-					int numDifference = intnumArr[i + 1] - intnumArr[i];
-
-				}
-
-			} else if (intnumArr[1] - intnumArr[0] < 0) { // 감소중
-				for (int i = 0; i < stnumArr.length - 1; i++) {
-					int numDifference = intnumArr[i + 1] - intnumArr[i];
-
-				}
-			}
-
-//			for (int i = 0; i < stnumArr.length - 1; i++) {
-//				int numDifference = intnumArr[i + 1] - intnumArr[i];
-//
-//				
-//
-//			}
-
-		}
-
-		return returnThis;
-	}
-
-	private static Scanner sc;
-
 	public static void main(String[] args) {
-
-		sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int resurt = 0;
-
-		for (int i = 1; i <= num; i++) {
-			if (hansooDiscriminant(i)) {
-				resurt += 1;
-			}
+		Scanner sc = new Scanner(System.in);
+		
+		// 입력구간
+		
+		int T = sc.nextInt(); // 테스트 개수 입력
+		String[] inputArr = new String[T];
+		
+		for (int i = 0; i < inputArr.length; i++) {
+			inputArr[i] = sc.next();
 		}
+		
+		// 처리 구간
+		
+		for (int i = 0; i < inputArr.length; i++) {
 
-		System.out.println("결괏값 : " + resurt);
-
+		}
+		
+		// 출력 구간
+		
+		
+		
 	}
 
 }
---
+++ ewrt/src/ewrt/test.java
@@ -0,0 +1,10 @@
+package ewrt;
+
+public class test {
+
+	public static void main(String[] args) throws Exception {
+		int a = System.in.read();
+		System.out.print(a);
+	}
+
+}
