df3737db55e144c86cfc27dd17f2ec1fcebe5686
ksj <gksjksjksjks@gmail.com>
2023-01-02 10:40:00 +0900

    완주못한선수찾기코드분석:실패

--- algorithm/src/Solution.java
+++ algorithm/src/Solution.java
@@ -1,42 +1,55 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-class Solution {
-	public String solution(String[] participant, String[] completion) {
-
-		// 문제 이해
-		// participant와 completion값을 비교 participant에 없는 값을 찾아 출력.
-
-		// 1. participant를 넣어 참가자 HashMap을 제작한다.
-
-		String answer = "";
-
-		Arrays.sort(participant);
-		Arrays.sort(completion);
-
-		int tmp = 0;
-		while (!participant[tmp].equals(completion[tmp++])) {
-			answer = participant[tmp-1];
+class Solution_Hash {
+	
+	// 1. 문제 단순화 하기
+	// 두 배열의 값을 비교하여 없는 값 1개를 찾으면 된다.
+	
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>(); // 해쉬 맵을 제작한다. 
+        
+        for (String player : participant) // 해쉬 맵에 배열크기 만큼 해쉬 맵에 데이터를 넣는다.
+        	
+        	map.put(player, map.getOrDefault(player, 0) + 1); // 해쉬 맵 key에 참가자 명을 넣고, value에 
+        	// key값이 있다 -> 동명이인이 있다. 그 수에 + 1을 한다. 
+        	// 없다 -> 0을 반환한다. -> + 1한다. 
+        	// 동명이인 = 2, 사람 = 1
+        	
+        
+        
+        for (String player : completion) 
+            map.put(player, map.get(player) - 1);
+        
+        	// 달성자는 -1을 해준다 
+        	// 완주자 = 0, 동명이인 1<=, 완주못함 = 1
+        	// 이때 동명이인이 3명이면 어떻게 해결하죠 ??
+        
+        for (int i = 0; i < map.size(); i++) {
+			
+        	
+        	
 		}
-//		
-//		HashMap<String, Integer> names = new HashMap<>();
-//		HashMap<String, Integer> finishNames = new HashMap<>();
+        
+
+//            Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
 //
-//		for (String name : participant) {
-//			names.put(name, tmp++);
-//		}
-
-		return answer;
-	}
-
-	public static void main(String[] args) {
-		Solution sl = new Solution();
-
-		String[] participant = { "leo", "kiki", "eden" }, completion = { "eden", "kiki" };
-
-		System.out.println(sl.solution(participant, completion));
-
-	}
-
+//        while(iter.hasNext()){
+//            Map.Entry<String, Integer> entry = iter.next();
+//            if (entry.getValue() != 0){
+//                answer = entry.getKey();
+//                break;
+//            }
+//        }
+        return answer;
+    }
+    
+    public static void main(String[] args){
+        String[] part = {"leo", "kiki", "eden"};
+        String[] comp = {"eden", "kiki"};
+        Solution_Hash sol = new Solution_Hash();
+        System.out.println(sol.solution(part, comp));
+    }
 }
--- algorithm/src/test.java
+++ algorithm/src/test.java
@@ -1,10 +1,20 @@
+import java.util.HashMap;
 
 public class test {
 	public static void main(String[] args) {
-		String a = "abc";
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(1, 3);
+		map.put(5, 7);
+		map.put(2, 4);
 		
-		System.out.println(a.substring(1, 2));
-		System.out.println(Integer.toString("Me".charAt(0) - 32));
-
+		System.out.println(map);
+		System.out.println(map.containsKey(3));
+		
+		System.out.println(map.containsValue(5));
+		System.out.println(map.get(5));
+		System.out.println(map.get(3));
+		System.out.println(map.getOrDefault(3, 3));
+		
+		
 	}
 }
